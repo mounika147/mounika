9fdfdc999859af89e514010556fc9ddc934fb04d
mounika147 <32008006+mounika147@users.noreply.github.com>
2017-09-16 11:58:04 +0530

    Add files via upload

---
+++ src/StudentGroup.java
@@ -0,0 +1,118 @@
+
+import java.util.Date;
+
+
+public class StudentGroup implements StudentArrayOperation {
+
+	private Student[] students;
+
+
+	public StudentGroup(int length) {
+		this.students = new Student[length];
+	}
+
+
+	public Student[] getStudents() {
+		// Add your implementation here
+		return students;
+	}
+
+
+	public void setStudents(Student[] students) {
+		this.students=students;// Add your implementation here
+	}
+
+
+	public Student getStudent(int index) {
+		// Add your implementation here
+		return students[index];
+	}
+
+
+	public void setStudent(Student student, int index) {
+		// Add your implementation here
+	}
+
+
+	public void addFirst(Student student) {
+		// Add your implementation here
+	}
+
+
+	public void addLast(Student student) {
+		// Add your implementation here
+	}
+
+
+	public void add(Student student, int index) {
+		// Add your implementation here
+	}
+
+
+	public void remove(int index) {
+		// Add your implementation here
+	}
+
+	public void remove(Student student) {
+		// Add your implementation here
+	}
+
+	public void removeFromIndex(int index) {
+		// Add your implementation here
+	}
+
+	public void removeFromElement(Student student) {
+		// Add your implementation here
+	}
+
+
+	public void removeToIndex(int index) {
+		// Add your implementation here
+	}
+
+
+	public void removeToElement(Student student) {
+		// Add your implementation here
+	}
+
+	public void bubbleSort() {
+		// Add your implementation here
+	}
+
+	public Student[] getByBirthDate(Date date) {
+		// Add your implementation here
+		return null;
+	}
+
+
+	public Student[] getBetweenBirthDates(Date firstDate, Date lastDate) {
+		// Add your implementation here
+		return null;
+	}
+
+	public Student[] getNearBirthDate(Date date, int days) {
+		// Add your implementation here
+		return null;
+	}
+
+
+	public int getCurrentAgeByDate(int indexOfStudent) {
+		// Add your implementation here
+		return 0;
+	}
+
+	public Student[] getStudentsByAge(int age) {
+		// Add your implementation here
+		return null;
+	}
+
+	public Student[] getStudentsWithMaxAvgMark() {
+		// Add your implementation here
+		return null;
+	}
+
+	public Student getNextStudent(Student student) {
+		// Add your implementation here
+		return null;
+	}
+}
